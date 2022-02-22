598623d7046c753c7036211f15d824ac78d866df
Vito-93 <62260870+Vito-93@users.noreply.github.com>
2022-02-22 11:33:52 +0100

    Update chart-area-demo.js

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -2,7 +2,7 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
-randomicValues = Array.from({length: 12}, () => Math.floor(Math.random() * 12));
+var randomicValues = Array.from({length: 12}, () => Math.round(Math.random()*99)).sort();
 
 function number_format(number, decimals, dec_point, thousands_sep) {
   // *     example: number_format(1234.56, 2, ',', ' ');
