6cd2d91a3cd4276f44f0fbd591ed1b6cfc8726a7
Vito-93 <62260870+Vito-93@users.noreply.github.com>
2022-02-22 11:39:50 +0100

    Update chart-area-demo.js

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -3,6 +3,7 @@ Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,Bli
 Chart.defaults.global.defaultFontColor = '#858796';
 
 var randomicValues = Array.from({length: 12}, () => Math.round(Math.random()*99)).sort();
+console.log(randomicValues);
 
 function number_format(number, decimals, dec_point, thousands_sep) {
   // *     example: number_format(1234.56, 2, ',', ' ');
